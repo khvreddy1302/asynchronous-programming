36c41405d3efda05b265165cd2fbc8ee78f98f8e
khvreddy1302 <kasaramharshavardhan@gmail.com>
2024-07-02 15:12:03 +0530

    Create async-basic.js

---
+++ async-basic.js
@@ -0,0 +1,13 @@
+console.log("starting");
+setTimeout(() =>{
+    console.log("2 seconds timer")
+},2000)
+
+setTimeout(() => {
+    console.log("0 second timer")
+    
+}, 0);
+
+
+
+console.log("stopping")
